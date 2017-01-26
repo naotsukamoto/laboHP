96eab4b77fd5c1c5107fc461605d61f6865813fd
nao tsukamoto <m0naaa0u@gmail.com>
2017-01-26 13:47:11 +0000

    ajax work well in memeber's page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,32 +43,21 @@ $(function() {
 });// button of backing to pagetop with bootstrap fin.
 
 // If teacher's button clicked...
+// ajax
+// ref: http://qiita.com/somewhatgood@github/items/113773747a6faa800366
+
+// glyphicon set to each button
 $(function(){
-  var stuBtn = $('#tea');
-  stuBtn.click(function(){
-
-
-
-
-
-    // ajax
-    // ref: http://qiita.com/somewhatgood@github/items/113773747a6faa800366
-    // $.ajax({
-    //       url: "users/show",
-    //       type: "GET",
-    //       data: { id: 1,
-    //             // mode: 'hoge',
-    //             // type: 'entry'
-    //             },
-    //       dataType: "html",
-    //       success: function(data) {
-    //           console.log("success");
-    //       },
-    //       error: function(data) {
-    //           console.log("errror");
-    //       }
-    //   });
-
-
-  });
+  for(var i=0;i<3;i++){
+    var span = document.createElement("span");
+    var btn = document.getElementById("btn-" + i);
+    if(i==0){
+      span.setAttribute("class", "glyphicon glyphicon-pencil");
+    }else if(i==1){
+      span.setAttribute("class", "glyphicon glyphicon-book");
+    }else{
+      span.setAttribute("class", "glyphicon glyphicon-user");
+    }
+    btn.appendChild(span);
+  }
 })
