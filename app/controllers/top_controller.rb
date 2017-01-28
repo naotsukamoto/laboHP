bbdc7085aeb85f37e88ad7b9393e06ad7198768b
nao tsukamoto <m0naaa0u@gmail.com>
2017-01-28 10:58:40 +0000

    search form work with ransack

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,9 @@
 class TopController < ApplicationController
   def index
     @page_title = "Top"
+    # @search=Head.search(content_cont: 'DB').result
     @search=Head.search(params[:q])
+    @results=@search.result
     @head=Head.all.order(:created_at => :DESC)
     @user=User.all
   end
